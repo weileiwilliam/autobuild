6199ab4494f0a518755f87e196f2c45b8609ba3d
weileiwilliam <407747458@qq.com>
2019-08-19 20:36:01 +0800

    fix

--- autobuild.sh
+++ autobuild.sh
@@ -1,12 +1,12 @@
 PATH=/home/www/$1
 type=${PATH##*-}
 
-echo $PATH
+echo $PATH $type
 
-# cd $PATH
-# git pull
-# cnpm install
-# if [ $type == "fontend" ]
-# then
-#   npm run build
-# fi
+cd $PATH
+git pull
+cnpm install
+if [ $type == "fontend" ]
+then
+  npm run build
+fi
--- index.js
+++ index.js
@@ -2,16 +2,15 @@ var http = require('http');
 var spawn = require('child_process').spawn;
 var createHandler = require('github-webhook-handler');
 
-// 下面填写的myscrect跟github webhooks配置一样，下一步会说；path是我们访问的路径
+// 下面填写的myscrect跟github webhooks配置一样
 var handler = createHandler({ path: '/autobuild', secret: 'forautobuild' });
 
 http.createServer(function (req, res) {
-  // console.log(req, res)
-  // handler(req, res, function (err) {
-  //   res.statusCode = 404;
-  //   res.end('no such location');
-  // })
-  runCommand('sh', ['./autobuild.sh', 'test-frontend']);
+  console.log(req, res)
+  handler(req, res, function (err) {
+    res.statusCode = 404;
+    res.end('no such location');
+  })
 }).listen(3000);
 
 handler.on('error', function (err) {
@@ -21,13 +20,16 @@ handler.on('error', function (err) {
 // 监听到push事件的时候执行我们的自动化脚本
 handler.on('push', function (event) {
   const fileName = event.payload.repository.name
-  console.log(fileName, 'fileName')
-  runCommand('sh', ['./autobuild.sh', fileName]);
+  runCommand('sh', ['./autobuild.sh', fileName], text => {
+    console.log(text)
+  });
 });
 
-function runCommand(cmd, args){
+function runCommand(cmd, args, callback){
   var child = spawn( cmd, args );
-  child.stdout.on('data', data => { console.log(data) })
-  child.stdout.on('error', function(err) { console.log(err || 'fail') });
+  let response = ''
+  child.stdout.on('data', function( buffer ){ response += buffer.toString() })
+  child.stdout.on('end', function(){ callback( response ) })
+  child.stdout.on('error', function(err) { console.log(err) });
 }
 
