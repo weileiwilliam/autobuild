2f5f7b555a1de5a5b13aa07c9b40610b65f23bde
weileiwilliam <407747458@qq.com>
2019-08-19 21:14:47 +0800

    fix

--- autobuild.sh
+++ autobuild.sh
@@ -1,16 +1,11 @@
 #! /bin/bash
 DIR_PATH=/home/www/$1
 type=${DIR_PATH##*-}
-
 echo $DIR_PATH $type
-# cd $PATH
-# ls
-# git pull
-# cnpm install
-# if [ $type == "fontend" ]
-# then
-#   npm run build
-# fi
-
-cd /home/www/test-frontend
+cd $DIR_PATH
 git pull
+cnpm install
+if [ $type == "fontend" ]
+then
+  npm run build
+fi
