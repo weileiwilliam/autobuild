ec058d09543c6610dfdde3075b5a15f094685e99
weileiwilliam <407747458@qq.com>
2019-08-19 20:53:43 +0800

    fix

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ handler.on('error', function (err) {
 // 监听到push事件的时候执行我们的自动化脚本
 handler.on('push', function (event) {
   const fileName = event.payload.repository.name
-  runCommand('sh', ['./autobuild.sh', fileName], text => {
+  runCommand('bash', ['./autobuild.sh', fileName], text => {
     console.log(text)
   });
 });
